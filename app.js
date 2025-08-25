cdd5a1fb5d2586d5b2e2ec3567f11222ff16aa7c
Matheus William <Matheus742w@gmail.com>
2025-08-25 14:00:34 -0400

    criação e sorteio entre da lista de amigos secretos.

--- app.js
+++ app.js
@@ -1,1 +1,44 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//criar lista para guardar os amigos
+let amigos = [];
+
+//função para adicionar o amigo
+function adicionarAmigo() {
+let amigosAdicionados = document.querySelector('input').value;
+    if (amigosAdicionados == ""){
+        alert('Digite um nome, por favor!');
+    } else {
+        amigos.push(amigosAdicionados);
+        limparCampo();
+        listaDeAmigos(amigos);
+    }
+}
+
+//função para limpar a lista
+function limparCampo(){
+    amigosAdicionados = document.querySelector('input');
+    amigosAdicionados.value = '';
+}
+
+//função para fazer a lista de amigos
+function listaDeAmigos(amigos){
+    let lista = document.querySelector('ul');
+    lista.innerHTML = '';
+
+    amigos.forEach(function(amigos) {
+        let novoAmigoAdicionado = document.createElement('li');
+        novoAmigoAdicionado.innerHTML = amigos;
+        lista.appendChild(novoAmigoAdicionado);
+    });    
+}
+
+//função para sortear o amigo 
+function sortearAmigo(){
+    if(amigos == ''){
+        alert('Você não possui amigos para sortear!!');
+    } else {
+        let indiceSorteio = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceSorteio];
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML= amigoSorteado;
+    }
+}
